f30f39d12a9604248b13eebcd7ba70dc31c6c49d
kylefuji <kylefuji63@gmail.com>
2026-02-27 23:44:57 -0700

    creating first level

--- Scripts/squid.gd
+++ Scripts/squid.gd
@@ -3,15 +3,17 @@ extends CharacterBody2D
 var start_mouse_distance:float = 0.0
 var mouse_hold:bool = false
 var moving:bool = false
-var distance_thresholds = [8, 16, 24, 32]
+var in_sand:bool = false
+var in_bubbles:bool = false
+var distance_thresholds = [16, 32, 48, 64]
 
 var speed:float = 10
 var original_speed:float = 0.0
 var max_speed:float = 669
+var speed_reduce:float = 500
 
 func _ready() -> void:
 	original_speed = speed
-
 
 func _physics_process(delta: float) -> void:
 	if mouse_hold:
@@ -29,8 +31,16 @@ func _physics_process(delta: float) -> void:
 		else:
 			$AnimatedSprite2D.play("sling4")
 	
+	var joystick_velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
 	if moving:
-		speed -= 500 * delta
+		if in_sand and not in_bubbles:
+			speed_reduce = 1500
+		elif in_bubbles and not in_sand:
+			speed_reduce = 250
+		else:
+			speed_reduce = 500
+		speed -= speed_reduce * delta
 		if speed <= 0:
 			moving = false
 			$AnimatedSprite2D.play("idle")
@@ -51,3 +61,23 @@ func _input(event: InputEvent) -> void:
 			var current_mouse_distance:float = get_global_mouse_position().distance_to(position) - start_mouse_distance
 			speed = min(current_mouse_distance * original_speed, max_speed)
 			moving = true
+			
+func death():
+	$DeathParticles.emitting = true
+	$AnimatedSprite2D.play("dead")
+
+
+func _on_sand_detector_body_entered(body: Node2D) -> void:
+	in_sand = true
+
+
+func _on_sand_detector_body_exited(body: Node2D) -> void:
+	in_sand = false
+
+
+func _on_bubble_detector_body_entered(body: Node2D) -> void:
+	in_bubbles = true
+
+
+func _on_bubble_detector_body_exited(body: Node2D) -> void:
+	in_bubbles = false
